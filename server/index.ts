772fbfab0f23c58e9b1f853de22a1a98c6e7070f
fromjaytoz <76098411+fromjaytoz@users.noreply.github.com>
2021-09-24 13:41:29 +0800

    set up index express boilerplate

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,20 @@
-console.log("test");
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(morgan("dev"));
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log("This server is running on port ", PORT);
+});
