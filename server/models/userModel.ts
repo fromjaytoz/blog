12abdf082bde22bfa4216d7bcb13854d3adca7ed
fromjaytoz <76098411+fromjaytoz@users.noreply.github.com>
2021-09-25 16:02:20 +0800

    add 'role' to userModel

--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -24,6 +24,10 @@ const user = new mongoose.Schema(
       default:
         "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png",
     },
+    role: {
+      type: String,
+      default: "user",
+    },
     type: {
       type: String,
       default: "normal",
