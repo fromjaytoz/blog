6478fc745492364ec11ce19821311de01314841d
fromjaytoz <76098411+fromjaytoz@users.noreply.github.com>
2021-09-25 16:57:54 +0800

    add google-auth-library, nodemailer dep

--- server/controllers/authCtrl.ts
+++ server/controllers/authCtrl.ts
@@ -2,28 +2,33 @@ import { Request, Response } from "express";
 import User from "../models/userModel";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { generateActiveToken } from "../config/generateTokens";
 
 const authCtrl = {
   register: async (req: Request, res: Response) => {
     try {
       const { name, account, password } = req.body;
       const user = await User.findOne({ account });
+
       if (user)
         return res.status(400).json({ msg: "Email/Phone already exists!" });
 
       const passwordHash = await bcrypt.hash(password, 12);
-      const newUser = new User({
-        name,
-        account,
-        password: passwordHash,
+      const newUser = { name, account, password: passwordHash };
+      const active_token = generateActiveToken({
+        name: newUser.name,
+        account: newUser.account,
+        password: newUser.password,
       });
-      newUser &&
+
+      if (new User(newUser).save()) {
         res.json({
           status: "OK",
           msg: "Registration was successful!",
           data: newUser,
-        }) &&
-        newUser.save();
+          active_token,
+        });
+      }
     } catch (err: any) {
       return res.status(500).json({ msg: err.message });
     }
